2abc248087239834caaca8f922f80b099c475d5f
Arthur Warmling <arthurwarmling1@gmail.com>
2026-07-26 00:08:13 -0300

    feat: adiciona novos estilos de UI e corrige a referência do script de lava

--- scenes/levels/lava_surface.gd
+++ scenes/levels/lava_surface.gd
@@ -1,7 +1,7 @@
 # lava.gd — no Sprite2D
 extends Sprite2D
 
-@export var scroll_speed := 20.0
+@export var scroll_speed := 15.0
 
 func _process(delta: float) -> void:
 	region_rect.position.x += scroll_speed * delta
